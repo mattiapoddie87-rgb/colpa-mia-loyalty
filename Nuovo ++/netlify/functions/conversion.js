d6f6a5cd81567d0a053169a5d773da5273d306cb
mattiapoddie87-rgb <mattia.poddie87@gmail.com>
2025-08-29 21:08:17 +0200

    Delete Nuovo ++ directory
    Delete Nuovo ++ directory.

--- Nuovo ++/netlify/functions/conversion.js
+++
@@ -1,6 +0,0 @@
-
-import { cfg } from './_common.js'
-export const handler = async () => {
-  const { eurPerMin } = cfg();
-  return { statusCode:200, headers:{'content-type':'application/json'}, body: JSON.stringify({ eur_per_min: eurPerMin }) };
-}
--- Nuovo ++/netlify/functions/get-site-url.js
+++
@@ -1,4 +0,0 @@
-
-export const handler = async () => {
-  return { statusCode:200, headers:{'content-type':'application/json'}, body: JSON.stringify({ site: process.env.SITE_URL || '' }) };
-}
